7ce8388c04a412b36265c6df438b66822a7ee282
Marek Guráň <skemgi2@gmail.com>
2022-12-25 18:24:03 +0100

    New design changes

--- app/src/main/java/com/marekguran/rozvrh/MainActivity.java
+++ app/src/main/java/com/marekguran/rozvrh/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        // odstranene kvoli schovanemu action bar v temach NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
